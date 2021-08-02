206a6cf8943735c3b9c52f7c13f83f08fa710178
Eman-Mohamed1 <eman.mohamed2648@gmail.com>
2021-08-02 14:01:00 +0200

    delete production line

--- server.js
+++ server.js
@@ -32,12 +32,7 @@ if(process.env.Node_ENV === "production"){
     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
   });
   
-}
 
-
-  app.get('/', (req, res) => {
-    res.send('Server is running');
-  });
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
@@ -52,6 +47,27 @@ app.get('/api/config/paypal', (req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
+
+}
+
+
+  app.get('/', (req, res) => {
+    res.send('Server is running');
+  });
+
+// app.use('/api/users', userRouter);
+// app.use('/api/products', productRouter);
+// app.use('/api/orders', orderRouter);
+// app.get('/api/config/paypal', (req, res) => {
+//   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+// });
+// // app.get('/', (req, res) => {
+// //   res.send('Server is ready');
+// // });
+
+// app.use((err, req, res, next) => {
+//   res.status(500).send({ message: err.message });
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
