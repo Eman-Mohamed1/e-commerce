84a0c69ba99847982fd80b73cbd132b9dbe750c5
Eman-Mohamed1 <eman.mohamed2648@gmail.com>
2021-08-02 14:06:30 +0200

    delete production line

--- server.js
+++ server.js
@@ -28,11 +28,16 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
 if(process.env.Node_ENV === "production"){
 
   app.use(express.static("frontend/build"));
-  app.get("*",(req,res)=>{
+  app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
   });
   
+}
 
+
+  app.get('/', (req, res) => {
+    res.send('Server is running');
+  });
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
@@ -47,27 +52,6 @@ app.get('/api/config/paypal', (req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
-}
-
-
-  app.get('/', (req, res) => {
-    res.send('Server is running');
-  });
-
-// app.use('/api/users', userRouter);
-// app.use('/api/products', productRouter);
-// app.use('/api/orders', orderRouter);
-// app.get('/api/config/paypal', (req, res) => {
-//   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-// });
-// // app.get('/', (req, res) => {
-// //   res.send('Server is ready');
-// // });
-
-// app.use((err, req, res, next) => {
-//   res.status(500).send({ message: err.message });
-// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
