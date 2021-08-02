856f8e3baf2056321f57098063f3fd4dbdceb533
Eman-Mohamed1 <eman.mohamed2648@gmail.com>
2021-08-02 07:11:21 +0200

    editab

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';//for sec info
 import productRouter from './backend/routers/productRouter.js';
 import userRouter from './backend/routers/userRouter.js';
 import orderRouter from './backend/routers/orderRouter.js';
-import path from 'path';
+// import path from 'path';
 
 
 dotenv.config();
-__dirname = path.resolve;
+// __dirname = path.resolve;
 const app = express();
 app.use(express.json());//midleware that parse body of post req so u can use it 
 app.use(express.urlencoded({ extended: true }));//midleware to attach body of post req to req.body 
@@ -27,7 +27,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
 
 if(process.env.Node_ENV === "production"){
 
-  app.use(express.static(path.join(__dirname,"frontend/build")));
+  app.use(express.static("frontend/build"));
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
   });
